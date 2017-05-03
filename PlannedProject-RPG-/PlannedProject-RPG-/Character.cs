f3fa1750c18726fe5d6d8c60b73905554c8faf20
jddrago <jason@delorie.com>
2017-05-03 16:30:17 -0600

    Base attributes added for characters

---
+++ PlannedProject-RPG-/PlannedProject-RPG-/Character.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlannedProject_RPG_
+{
+    class Character
+    {
+        private int baseHP, currentHP, baseMP, currentMP, STR, DEX, INT, STRMod, DEXmod, INTmod, damageBonus, strikeBonus, dodgeBonus, spellBonus;
+        private string name;
+    }
+}
---
+++ PlannedProject-RPG-/PlannedProject-RPG-/Hero.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlannedProject_RPG_
+{
+    class Hero : Character
+    {
+    }
+}
