17c88499090ea3deb4efb66782ecd149808cee0b
ZFlem96 <zackzacharyfleming@gmail.com>
2017-05-08 16:19:34 -0600

    everything i've done with potions and weapons

---
+++ PlannedProject-RPG-/PlannedProject-RPG-/HealthPotion.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlannedProject_RPG_
+{
+    class HealthPotion
+    {
+        private int value = 0;
+        public HealthPotion()
+        {
+            int valueNum = 0;
+            Random rand = new Random();
+            valueNum = rand.Next(1,11);
+            value = valueNum + (valueNum * 10);
+        }
+
+        public int Use()
+        {
+            int valueNum = value;
+            value = 0;
+            return valueNum;
+        }
+
+
+    }
+}
