9dda35c89c0099cd33517b5eae4f9b5a34ecadba
ZFlem96 <zackzacharyfleming@gmail.com>
2017-05-09 16:31:21 -0600

    improved Inventory.cs

--- PlannedProject-RPG-/PlannedProject-RPG-/Inventory.cs
+++ PlannedProject-RPG-/PlannedProject-RPG-/Inventory.cs
@@ -8,5 +8,80 @@ namespace PlannedProject_RPG_
 {
     class Inventory
     {
+        private HealthPotion[] healthPotions;
+        private MagicPotion[] magicPotions;
+        private Weapon[] weapons;
+
+        public int useHealthPotion()
+        {
+            int healthpotion = 0;
+            for (int x = 0; x<healthPotions.Length;x++)
+            {
+                if (healthPotions[x]!=null)
+                {
+                    healthpotion = healthPotions[x].Use();
+                    healthPotions[x] = null;
+                    break;
+                }
+            }
+            return healthpotion;
+        }
+
+        public void addHealthPotion(HealthPotion potion)
+        {
+            for (int x = 0; x<healthPotions.Length;x++)
+            {
+                if (healthPotions[x]==null)
+                {
+                    healthPotions[x] = new HealthPotion();
+                    break;
+                }
+            }
+        }
+
+        public int useMagicPotion()
+        {
+            int potion = 0;
+            for (int x = 0; x < magicPotions.Length; x++)
+            {
+                if (magicPotions[x] != null)
+                {
+                    potion = magicPotions[x].Use();
+                   magicPotions[x] = null;
+                    break;
+                }
+            }
+            return potion;
+        }
+
+        public void addMagicPotion(MagicPotion potion)
+        {
+            for (int x = 0; x < magicPotions.Length; x++)
+            {
+                if (magicPotions[x] == null)
+                {
+                    magicPotions[x] = new MagicPotion();
+                    break;
+                }
+            }
+        }
+
+        public Weapon getWeaponAtIndex(int index)
+        {
+            return weapons[index];
+        }
+
+        public void addWeapon(Weapon weapon)
+        {
+            for (int x = 0; x<weapons.Length;x++)
+            {
+                if (weapons[x]==null)
+                {
+                    weapons[x] = weapon;
+                    break;
+                }
+            }
+            
+        }
     }
 }
