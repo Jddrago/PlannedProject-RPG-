cfd351ad09f6ce3bd9d694b4be160b9c64d26a8f
jddrago <jason@delorie.com>
2017-05-09 21:32:49 -0600

    modified inventory

--- PlannedProject-RPG-/PlannedProject-RPG-/Inventory.cs
+++ PlannedProject-RPG-/PlannedProject-RPG-/Inventory.cs
@@ -9,61 +9,37 @@ namespace PlannedProject_RPG_
     class Inventory
     {
         private static int numPotions = 5,numWeapons = 3;
-        private HealthPotion[] healthPotions = new HealthPotion[numPotions];
-        private MagicPotion[] magicPotions = new MagicPotion[numPotions];
+        private List<HealthPotion> healthPotions = new List<HealthPotion>(numPotions);
+        private List<MagicPotion> magicPotions = new List<MagicPotion>(numPotions);
         private Weapon[] weapons = new Weapon[numWeapons];
 
         public int useHealthPotion()
         {
-            int healthpotion = 0;
-            for (int x = 0; x<healthPotions.Length;x++)
-            {
-                if (healthPotions[x]!=null)
-                {
-                    healthpotion = healthPotions[x].Use();
-                    healthPotions[x] = null;
-                    break;
-                }
-            }
-            return healthpotion;
+            HealthPotion hp = healthPotions.Last();
+            healthPotions.RemoveAt(healthPotions.Count());
+            return hp.Use();
         }
 
         public void addHealthPotion(HealthPotion potion)
         {
-            for (int x = 0; x<healthPotions.Length;x++)
+            if (healthPotions.Count() < 5)
             {
-                if (healthPotions[x]==null)
-                {
-                    healthPotions[x] = new HealthPotion();
-                    break;
-                }
+                healthPotions.Add(potion);
             }
         }
 
         public int useMagicPotion()
         {
-            int potion = 0;
-            for (int x = 0; x < magicPotions.Length; x++)
-            {
-                if (magicPotions[x] != null)
-                {
-                    potion = magicPotions[x].Use();
-                   magicPotions[x] = null;
-                    break;
-                }
-            }
-            return potion;
+            MagicPotion mp = magicPotions.Last();
+            magicPotions.RemoveAt(magicPotions.Count());
+            return mp.Use();
         }
 
         public void addMagicPotion(MagicPotion potion)
         {
-            for (int x = 0; x < magicPotions.Length; x++)
+            if (magicPotions.Count() < 5)
             {
-                if (magicPotions[x] == null)
-                {
-                    magicPotions[x] = new MagicPotion();
-                    break;
-                }
+                magicPotions.Add(potion);
             }
         }
 
@@ -86,28 +62,12 @@ namespace PlannedProject_RPG_
 
         public int numHealthPotions()
         {
-            int temp = 0;
-            for (int i = 0; i < healthPotions.Length; i++)
-            {
-                if (healthPotions[i] != null)
-                {
-                    temp++;
-                }
-            }
-            return temp;
+            return healthPotions.Count();
         }
 
         public int numMagicPotions()
         {
-            int temp = 0;
-            for (int i = 0; i < magicPotions.Length; i++)
-            {
-                if (magicPotions[i] != null)
-                {
-                    temp++;
-                }
-            }
-            return temp;
+            return magicPotions.Count();
         }
     }
 }
